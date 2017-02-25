305391a683e747f221b6920ea843ae19ea4be491
sgcastle <sam.gordon.castle@gmail.com>
2017-02-24 17:48:51 -0800

    Adding bullet functionality

--- settings.py
+++ settings.py
@@ -9,4 +9,11 @@ class Settings():
 		self.bg_color = (230, 230, 230)
 
 		# Ship settings
-		self.ship_speed_factor = 5.5
+		self.ship_speed_factor = 2.5
+
+		# Bullet settings
+		self.bullet_speed_factor = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60, 60, 60
+		self.bullets_allowed = 3
