3121e3e3ae92c046a8ffa6eabcdceb46e7e3d0d8
sgcastle <sam.gordon.castle@gmail.com>
2017-02-26 13:51:41 -0800

    Adding bullet collision and reseting alien fleet functionality

--- game_functions.py
+++ game_functions.py
@@ -51,15 +51,25 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 	# Make the most recently drawn screen visible
 	pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
 	"""Update position of bullets and get rid of old bullets."""
 	# Update Bullet positions
 	bullets.update()
-
 	# Get rid of bullets tha have disappeared
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+	check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+	"""Respond to bullet-alien collisions."""
+	# Remove any bullets and aliens that have collided
+	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+	if len(aliens) == 0:
+		# Destroy existing bullets and create new fleet
+		bullets.empty()
+		create_fleet(ai_settings, screen, ship, aliens)
+
 
 def get_number_of_aliens_x(ai_settings, alien_width):
 	"""Determine the number of aliens that fit in a row."""
